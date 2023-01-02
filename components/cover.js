eda10948d90eec43050fb764297faa016fb497f0
Glenn Packer <glenn.packer@glenn-packer.net>
2023-01-02 14:20:12 +0100

    BEAUX-4
    BEAUX-4 translations routing

--- components/cover.js
+++ components/cover.js
@@ -1,24 +1,52 @@
 import Image from 'next/image'
 import styles from '../styles/cover.module.css'
 import Link from 'next/link';
+import { useRouter } from 'next/router'
 
 export default function Cover({ data: { src, title, alt, link, textClass = '', para = ''} }) {
-    return (
-        <Link href='/'>
-            <div className={ styles.imageContainer } >
+    const router = useRouter();
 
-                <Image 
-                    className={ styles.image } 
-                    src={ src }
-                    height={400}
-                    width={1300}
-                    alt={ alt }
-                />
-                <div className={ `${styles.centered} ${ textClass ? styles[textClass] : ''}` }>
-                    <h1 className={ styles.title }>{ title }</h1>
-                    <div>{ para }</div>
-                </div>
+    return (
+        <>
+            <div className={ styles.flagContainer } >
+                { router.locale !== 'en' &&
+                    <Link href={ router.route } locale="en">
+                        <Image 
+                            className={ styles.flag } 
+                            src="/unionjack.jpg"
+                            height={39}
+                            width={75}
+                            alt="French"
+                        />
+                    </Link>
+                }
+                { router.locale !== 'fr' &&
+                    <Link href={ router.route } locale="fr">
+                        <Image 
+                            className={ styles.flag } 
+                            src="/tricolore.jpg"
+                            height={39}
+                            width={75}
+                            alt="English"
+                        />
+                    </Link>
+                }
             </div>
-        </Link>
+            <Link href='/'>
+                <div className={ styles.imageContainer } >
+                    <Image 
+                        className={ styles.image } 
+                        src={ src }
+                        height={400}
+                        width={1300}
+                        alt={ alt }
+                    />
+                    <div className={ `${styles.centered} ${ textClass ? styles[textClass] : ''}` }>
+                        <h1 className={ styles.title }>{ title }</h1>
+                        <div>{ para }</div>
+                    </div>
+                </div>
+            </Link>
+        </>
     )
 }
