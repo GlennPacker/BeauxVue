1a9a20fc8eb74e8201031cd345303553c2d0f9d5
Glenn Packer <glenn.packer@glenn-packer.net>
2023-01-06 17:56:17 +0100

    fixed issue with overlaps

--- components/lakePage.js
+++ components/lakePage.js
@@ -5,39 +5,24 @@ import Cover from '../components/cover';
 import Lake from '../components/lake';
 
 
-export default function LakePage({schema}) {
-    return (
-        <>
-          <Head>
-            <title>{ schema.pageTitle }</title>
-            <meta name="description" content={ schema.description } />
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
+export default function LakePage({ schema }) {
+  return (
+    <>
+      <Head>
+        <title>{schema.pageTitle}</title>
+        <meta name="description" content={schema.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-          <Cover data={schema.cover} />
+      <Cover data={schema.cover} />
 
-          <div>
-            <Lake data={ schema.lake }/>
-          </div>
+      <div>
+        <Lake data={schema.lake} />
+      </div>
 
-          <main className={styles.main }>
-            <CtaList ctaData={schema.cta}></CtaList>
-          </main>
-
-          
-          {/* <footer className={styles.footer}>
-              contact
-              <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              >
-              Powered by{' '}
-              <span className={styles.logo}>
-                  <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-              </span>
-              </a>
-          </footer> */}
-        </>
-    );
+      <main className={styles.main}>
+        <CtaList ctaData={schema.cta}></CtaList>
+      </main>
+    </>
+  );
 }
